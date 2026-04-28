0486f8c2b0481ec26cd2465afdb9e64f69859998
Gus <101070070+Gust4v1n@users.noreply.github.com>
2026-04-28 11:34:31 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,24 @@
-const CACHE_NAME = 'pizza-cache-v8';
+const CACHE_NAME = 'Restaurante-mexicano-cache-v8';
 const ASSETS = [
-    './',
-    './index.html',
-    './manifest.json',
-    './assets/images/logoPizza.png',
-    './assets/images/pizzaBanner_PNG.png',
-    './assets/images/whatsapp.png'
+    '/',
+    '/index.html',
+    'manifest.json',
+    '/assets/images/logoPizza.png',
+    '/assets/images/pizzaBanner_PNG.png',
+    '/assets/images/whatsapp.png',
 ]
-
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) =>{
-            console.log('Fatiando a pizza no cache');
-            return cache.addAll(ASSETS)
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Preparando');
+            return cache.addAll(ASSETS);
         })
-    )
-});
-
-self.addEventListener('activate', (event)=>{
-    event.waitUntil(
-            caches.keys().then((keys)=>{
-                return Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))
-            );
-        })
-    )
+    );
 })
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
