58b45624b0fe934119fff94dfc93c5e88a06f826
Gus <101070070+Gust4v1n@users.noreply.github.com>
2026-04-28 11:25:54 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -16,3 +16,12 @@ self.addEventListener('install', (event) => {
         })
     )
 });
+
+self.addEventListener('activate', (event)=>{
+    event.waitUntil(
+            caches.keys().then((keys)=>{
+                return Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))
+            );
+        })
+    )
+})
